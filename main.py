4881e2b7178ef1de0b4140300c06c6debf23c9bb
EliseevDV <109437194+EliseevDV@users.noreply.github.com>
2023-02-13 16:21:18 +0000

    Solved the issue with duplicates

--- main.py
+++ main.py
@@ -1,45 +1,51 @@
-from pprint import pprint
 import csv
 import re
 
 with open("phonebook_raw.csv", encoding='utf8') as f:
-  rows = csv.reader(f, delimiter=",")
-  contacts_list = list(rows)
-pprint(contacts_list)
+    rows = csv.reader(f, delimiter=",")
+    contacts_list = list(rows)
 
-# TODO 1: выполните пункты 1-3 ДЗ
-def fix_address_book(contacts_list):
-    fixed_rows = []
+def merge_duplicates(contacts_list):
+    unique_data = {}
+    for item in contacts_list:
+        name = item[0]
+
+        if name in unique_data:
+            unique_item = unique_data[name]
+            for key, value in item.items():
+                if key != 'Name' and value and not unique_item.get(key):
+                    unique_item[key] = value
+        else:
+            unique_data[name] = item
+    return list(unique_data.values())
+
+
+def repair_book(contacts_list):
+    contacts_list = merge_duplicates(contacts_list)
+    repaired_book = []
+    seen_contacts = set()
     for row in contacts_list:
-        name_parts = re.split('\s+', row[0])
-        lastname = name_parts[0]
-        firstname = name_parts[1] if len(name_parts) > 1 else ''
-        surname = name_parts[2] if len(name_parts) > 2 else ''
+        full_name = re.sub(r'\s+', ' ', row[0].strip())
+        last_name, first_name, surname = re.split(r'\s+', full_name, maxsplit=2) + [None, None, None][len(re.split(r'\s+', full_name,maxsplit=2)):]
         phone = re.sub(r'\D', '', row[5])
         phone = '+7({}) {}-{}-{}'.format(phone[1:4], phone[4:7], phone[7:9], phone[9:11])
-        if len(phone) > 11:
-            phone += ' доб.{}'.format(phone[11:])
-        email = row[6] if row[6] else ''
-        for c in fixed_rows:
-            if c[0] == lastname and c[1] == firstname:
-                c[2] = surname if surname else c[2]
-                c[5] = phone if phone else c[5]
-                c[6] = email if email else c[6]
-                break
-        else:
-            fixed_rows.append([lastname, firstname, surname, row[3], row[4], phone, email])
-    return fixed_rows
+        if len(row[5]) > 11:
+            phone += ' доб.' + row[5][11:]
+            email = row[6].lower()
+            position = row[4].strip().lower()
+            contact_info = (last_name, first_name, surname, email, phone, position)
+            if contact_info in seen_contacts:
+                continue
+            seen_contacts.add(contact_info)
+            repaired_book.append([last_name, first_name, surname] + row[1:5] + [phone, email])
+    return repaired_book
 
 
-fixed_rows = fix_address_book(contacts_list)
-header = ["lastname", "firstname", "surname", "organization", "position", "phone", "email"]
-fixed_rows.insert(0, header)
+fixed_rows = repair_book(contacts_list)
 
-
-
-# TODO 2: сохраните получившиеся данные в другой файл
-# код для записи файла в формате CSV
 with open("fixed_contacts.csv", "w", newline="", encoding='utf8') as f:
     writer = csv.writer(f)
     writer.writerows(fixed_rows)
+
+
 
